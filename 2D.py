65aaf7e0fc2639ab6279b203c63b42f5903a0f99
YuLei-1005203115 <1005203115@cugb.edu.cn>
2022-11-28 20:09:22 +0800

    upgrade

--- 2D.py
+++ 2D.py
@@ -1,8 +1,3 @@
-import numpy as np
-import numpy.linalg as nla
-import matplotlib.pyplot as plt
-from matplotlib import cm
-from mpl_toolkits.mplot3d import Axes3D
 from tkinter import *
 import hydraulicheadsimulate as hs
 
